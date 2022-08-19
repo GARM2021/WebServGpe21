3d36ac818a71443f10cedc7985bc243dd8a39018
GARM2021 <ivanhoe.real@gmail.com>
2022-08-19 13:37:50 -0500

    C30

--- app/Http/Controllers/api/DefaultbController.php
+++ app/Http/Controllers/api/DefaultbController.php
@@ -54,31 +54,6 @@ class DefaultbController extends Controller
 		// require('conecta.php');
 
         //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-//Configuracion de la conexion a base de datos LOCAL
-		// $bd_host = "207.248.62.188";  //ip del web
-
-//$bd_usuario = "tesoreria"; 
-//$bd_password = "#tecinf2017"; 
-
-		// $bd_usuario = "sa"; 
-		// $bd_password = "Gu4d4Lup317"; 
-
-		// $bd_base = "gpe".trim(date("Y"));
-
-		// $con = mssql_connect($bd_host, $bd_usuario, $bd_password) or die('no se puede conectar a SQL Server'); 
-		// mssql_select_db($bd_base,$con); 
-
-
-		//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-		//Configuracion de la conexion a base de datos LOCAL
-		// $bd_host = "207.248.62.188";  //ip del web
-		// //$bd_host = "192.10.228.10"; 
-
-		// //$bd_host = "64.22.119.64"; 
-
-		// $bd_usuario = "sa"; 
-		// $bd_password = "Gu4d4Lup317"; 
 
 		// //$bd_usuario = "sa_pago_predial"; 
 		// //$bd_password = "InfoWeb2013"; 
